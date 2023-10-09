af0b0835ada247f24c20249845b516d7655c1687
liyijia <liyijiadou@gmail.com>
2023-10-09 11:09:07 +0300

    update: lab3, new try

--- lab3/Client/src/main/java/com/client/ClientApplication.java
+++ lab3/Client/src/main/java/com/client/ClientApplication.java
@@ -1,13 +1,89 @@
 package com.client;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.TrustSelfSignedStrategy;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.http.HttpClient;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 @SpringBootApplication
 public class ClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
+    }
+
+
+    @Bean
+    public RestTemplate getRestTemplate() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, KeyManagementException, UnrecoverableKeyException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = null;
+
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+
+        try (CloseableHttpClient httpClient = httpClientBuilder.setConnectionManager(getHttpClientConnectionManager()).build()) {
+
+//        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(new SSLContextBuilder()
+//                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+//                .loadKeyMaterial(keyStore, "password".toCharArray()).build(),
+//                NoopHostnameVerifier.INSTANCE);
+//
+//        HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory)
+//                .setMaxConnTotal(Integer.valueOf(5))
+//                .setMaxConnPerRoute(Integer.valueOf(5))
+//                .build();
+
+        requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+//            requestFactory.setReadTimeout(Integer.valueOf(10000));
+        requestFactory.setConnectTimeout(Integer.valueOf(10000));
+
+        restTemplate.setRequestFactory(requestFactory);
+        }
+        return restTemplate;
+    }
+
+    private static HttpClientConnectionManager getHttpClientConnectionManager() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException, UnrecoverableKeyException, CertificateException, IOException {
+        return PoolingHttpClientConnectionManagerBuilder.create()
+                .setSSLSocketFactory(getSslConnectionSocketFactory())
+                .build();
+    }
+
+    /**
+     * 支持SSL
+     *
+     * @return SSLConnectionSocketFactory
+     */
+    private static SSLConnectionSocketFactory getSslConnectionSocketFactory() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException, CertificateException, UnrecoverableKeyException {
+//        TrustStrategy acceptingTrustStrategy = (x509Certificates, s) -> true;
+        KeyStore keyStore = KeyStore.getInstance("jks");
+        ClassPathResource classPathResource = new ClassPathResource("gateway.jks");
+        InputStream inputStream = classPathResource.getInputStream();
+        keyStore.load(inputStream, "password".toCharArray());
+        SSLContext sslContext = SSLContexts.custom()
+                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+                .loadKeyMaterial(keyStore, "password".toCharArray())
+                .build();
+        return new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
     }
 
 }
--- lab3/Client/src/main/java/com/client/RestTemplateConfig.java
+++ lab3/Client/src/main/java/com/client/RestTemplateConfig.java
@@ -1,66 +1,71 @@
-package com.client;
-import org.apache.hc.client5.http.classic.HttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
-import org.apache.hc.core5.ssl.SSLContextBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.SSLContext;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-
-/**
- * @author liyijia
- * @create 2023-10-2023/10/8
- */
-@Configuration
-public class RestTemplateConfig {
-
-    @Value("${server.ssl.pure-key-store}")
-    public String keyStore;
-    @Value("${server.ssl.key-store-password}")
-    public String keyStorePassword;
-    @Value("${server.ssl.pure-trust-store}")
-    public String trustStore;
-    @Value("${server.ssl.trust-store-password}")
-    public String trustStorePassword;
-
-    @Bean
-    public RestTemplate restTemplate(ClientHttpRequestFactory httpComponentsClientHttpRequestFactory) {
-        RestTemplate restTemplate = new RestTemplate(httpComponentsClientHttpRequestFactory);
-        restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
-//        log.info("loading restTemplate");
-        return restTemplate;
-    }
-
-    @Bean("httpComponentsClientHttpRequestFactory")
-    public ClientHttpRequestFactory httpComponentsClientHttpRequestFactory() throws IOException, UnrecoverableKeyException, NoSuchAlgorithmException,
-            KeyStoreException, KeyManagementException, CertificateException {
-        SSLContext sslContext = SSLContextBuilder
-                .create()
-                // 作为client，load自己密钥库；理论上当server的server.ssl.client-auth=need时，通过它可以获取client的公钥证书传递给server来验证
-                .loadKeyMaterial(new ClassPathResource(keyStore).getURL(),
-                        trustStorePassword.toCharArray(), keyStorePassword.toCharArray())
-                // 作为client，load自己的信任库；在请求server之前，client先通过它判断server是否受信
-                .loadTrustMaterial(new ClassPathResource(trustStore).getURL(), trustStorePassword.toCharArray())
-                .build();
-        HttpClient client = HttpClients.custom()
-                .setSSLContext(sslContext)
-                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-                .build();
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(client);
-        return requestFactory;
-    }
-}
+//package com.client;
+//import org.apache.hc.client5.http.classic.HttpClient;
+//import org.apache.hc.client5.http.impl.classic.HttpClients;
+//import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+//import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+//import org.apache.hc.client5.http.ssl.TrustSelfSignedStrategy;
+//import org.apache.hc.core5.ssl.SSLContextBuilder;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.http.client.ClientHttpRequestFactory;
+//import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+//import org.springframework.http.converter.StringHttpMessageConverter;
+//import org.springframework.web.client.RestTemplate;
+//
+//import javax.net.ssl.SSLContext;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.nio.charset.StandardCharsets;
+//import java.security.*;
+//import java.security.cert.CertificateException;
+//
+///**
+// * @author liyijia
+// * @create 2023-10-2023/10/8
+// *
+// */
+//@Configuration
+//public class RestTemplateConfig {
+//
+//    public String keyStore = "gateway.jks";
+//    public String keyStoreType = "jks";
+//    public String password = "passwd";
+//
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        RestTemplate restTemplate = new RestTemplate();
+//        KeyStore keyStore;
+//        HttpComponentsClientHttpRequestFactory requestFactory = null;
+//
+//        try {
+//            keyStore = KeyStore.getInstance(this.keyStoreType);
+//            ClassPathResource classPathResource = new ClassPathResource(this.keyStore);
+//            InputStream inputStream = classPathResource.getInputStream();
+//            keyStore.load(inputStream, "passwd".toCharArray());
+//
+//            SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
+//                    new SSLContextBuilder().loadTrustMaterial(null, new TrustSelfSignedStrategy())
+//                    .loadKeyMaterial(keyStore, this.password.toCharArray()).build(),
+//                    NoopHostnameVerifier.INSTANCE);
+//
+//            HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory)
+//                    .setMaxConnTotal(Integer.valueOf(5))
+//                    .setMaxConnPerRoute(Integer.valueOf(5))
+//                    .build();
+//
+//            requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+//            requestFactory.setReadTimeout(Integer.valueOf(10000));
+//            requestFactory.setConnectTimeout(10000);
+//
+//            restTemplate.setRequestFactory(requestFactory);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return restTemplate;
+//    }
+//
+//
+//}
